16cef79172d39375cf2dcef4c0358284790a06a8
sachin123jadhav <sachin123jadhav@gmail.com>
2023-10-04 17:51:09 +0530

    updation according to doc file

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,3 +132,30 @@ $('.testimonial-carousel').owlCarousel({
 });
 
 });
+
+
+
+ /*===================================
+         LATEST ARRIVALS OWL CAROUSEL
+    ======================================*/
+    $('.lastest_featured_products').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
